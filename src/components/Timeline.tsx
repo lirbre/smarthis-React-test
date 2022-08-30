5b1d330a268b430d058ae78b50c2051963d2c0eb
Breno Lira <brenoliradev@gmail.com>
2022-08-30 18:04:37 -0300

    chore: test reload button

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,81 +1,47 @@
-/* eslint-disable */
-import { MySession } from "@/typings/session";
-import { useSession } from "next-auth/react";
-import { useEffect, useMemo, useState } from "react";
-import { CustomEmbed } from "./CustomEmbed";
-import InfiniteScroll from "react-infinite-scroll-component";
+import { useMemo, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
-interface TweetData {
-  created_at: string;
-  id: string;
-  author_id: string;
-  text: string;
-  public_metrics: {
-    retweet_count: number;
-    reply_count: number;
-    like_count: number;
-    quote_count: number;
-  };
-  referenced_tweets: [
-    {
-      type: string;
-      id: string;
-    }
-  ];
+import { TweetData } from '@/typings/tweets'
+
+import { CustomEmbed } from './CustomEmbed'
+
+interface TweetDataProps {
+  tweetData: TweetData[]
+  isFetching: boolean
 }
 
-export const Timeline = () => {
-  const { data } = useSession();
-  const myData: MySession = data as MySession;
-  const [tweetData, setData] = useState<TweetData[]>([]);
-  const [shouldShow, setShouldShow] = useState<number>(8);
-  const [isFetching, setIsFetching] = useState<boolean>(true);
-
-  useEffect(() => {
-    if (!myData) return;
-    getTweets();
-  }, [myData]);
-
-  const getTweets = () => {
-    fetch(`/api/tweets/${myData?.user?.uid}`)
-      .then((res) => res.json())
-      .then((result) => {
-        setData(result.data);
-        setIsFetching(false);
-      });
-  };
+export const Timeline = ({ tweetData, isFetching }: TweetDataProps) => {
+  const [shouldShow, setShouldShow] = useState<number>(8)
 
   const showCorrectly = useMemo(
     () => [...tweetData].slice(0, shouldShow),
     [shouldShow, tweetData]
-  );
+  )
 
   const memoizedTweets = useMemo(
     () => showCorrectly?.map(({ id }) => <CustomEmbed key={id} id={id} />),
     [showCorrectly]
-  );
+  )
 
   return (
     <div className="w-full">
       {!isFetching && tweetData.length === 0 ? (
         <div className="w-full items-center justify-center pb-32">
-        <p className="w-full text-center font-bold text-[#000]">
-          Você não tem nenhum tweet disponível.
-        </p>
-      </div>
+          <p className="w-full text-center font-bold text-[#000]">
+            Você não tem nenhum tweet disponível.
+          </p>
+        </div>
       ) : (
         <InfiniteScroll
           dataLength={shouldShow}
           next={() => setShouldShow((prev) => prev + 10)}
           hasMore={shouldShow < tweetData.length}
-          loader={
-            <></>
-          }
-          className="grid lg:grid-cols-2 grid-cols-1 grid-flow-row-dense gap-12 justify-center pb-16 md:px-6 w-full mx-auto"
+          loader={<></>}
+          className="mx-auto grid w-full grid-flow-row-dense grid-cols-1 justify-center gap-12 pb-16 md:px-6 lg:grid-cols-2"
         >
           {memoizedTweets}
         </InfiniteScroll>
       )}
     </div>
-  );
-};
+  )
+}
--- src/components/WithUser.tsx
+++ src/components/WithUser.tsx
@@ -1,6 +1,31 @@
+import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+
+import { MySession } from '@/typings/session'
+import { TweetData } from '@/typings/tweets'
+
 import { Timeline } from './Timeline'
 
 export const WithUser = () => {
+  const { data } = useSession()
+  const myData: MySession = data as MySession
+  const [tweetData, setTweetData] = useState<TweetData[]>([])
+  const [isFetching, setIsFetching] = useState<boolean>(true)
+
+  const getTweets = () => {
+    fetch(`/api/tweets/${myData?.user?.uid}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setTweetData(result.data)
+        setIsFetching(false)
+      })
+  }
+
+  useEffect(() => {
+    if (!myData) return
+    getTweets()
+  }, [myData])
+
   return (
     <div className="mx-auto flex w-full flex-col items-center justify-center gap-16">
       <div className="flex w-full flex-col items-center justify-center lg:flex-row">
@@ -11,6 +36,12 @@ export const WithUser = () => {
           <p className="mr-auto max-w-[500px] text-left font-normal text-[#000]">
             Veja seus últimos 100 Tweets abaixo.
           </p>
+          <button
+            className="mr-auto border-4 border-black bg-[#fff] px-8 py-4 shadow-[8px_8px_0_#000] transition duration-200 ease-in hover:-translate-y-1 hover:-translate-x-1 hover:shadow-[12px_12px_0_#000] md:px-12 md:py-6"
+            onClick={() => getTweets()}
+          >
+            <small className="font-black text-[#000]">RELOAD</small>
+          </button>
         </div>
         <object
           type="image/svg+xml"
@@ -18,7 +49,7 @@ export const WithUser = () => {
           className="mt-6 w-11/12 max-w-[638px] pb-12"
         />
       </div>
-      <Timeline />
+      <Timeline isFetching={isFetching} tweetData={tweetData} />
     </div>
   )
 }
