791ca2b67350e27730ce39fa4e9485f6341127b5
Breno Lira <brenoliradev@gmail.com>
2022-08-30 13:28:23 -0300

    fix: timeline position

--- src/components/CustomEmbed.tsx
+++ src/components/CustomEmbed.tsx
@@ -11,7 +11,7 @@ export const CustomEmbed = ({ id }: CustomEmbedProps) => {
   const [isLoading, setLoading] = useState<boolean>(true)
 
   return (
-    <div className="flex justify-center" style={{ animation: 'fade-in .5s' }}>
+    <div className="mx-auto w-full" style={{ animation: 'fade-in .5s' }}>
       <div style={{ animation: `${!isLoading ? 'fade-in .5s' : ''}` }}>
         <TwitterTweetEmbed onLoad={() => setLoading(false)} tweetId={id} />
       </div>
